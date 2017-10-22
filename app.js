36975c19eea1e212f1f98db53bda3214d7459907
Arden Kim <kmk3170@uw.edu>
2017-10-22 07:21:06 -0700

    parse video id, add icon, clean code

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 "use strict";
 
-var videoUrl = "http://video.google.com/timedtext?lang=fr&v=zCaxXQdkfPk";
-var msURL = "https://api.microsofttranslator.com/V2/Http.svc";
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vYXBpLm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInN1YnNjcmlwdGlvbi1pZCI6IjY4ZDE3ODQ4YmRiMjRhNDlhZGM0YmE1NjJkOWIxMjVlIiwicHJvZHVjdC1pZCI6IlRleHRUcmFuc2xhdG9yLkYwIiwiY29nbml0aXZlLXNlcnZpY2VzLWVuZHBvaW50IjoiaHR0cHM6Ly9hcGkuY29nbml0aXZlLm1pY3Jvc29mdC5jb20vaW50ZXJuYWwvdjEuMC8iLCJhenVyZS1yZXNvdXJjZS1pZCI6Ii9zdWJzY3JpcHRpb25zLzBkMmZmOThjLTVkYjAtNGZiOC05MmI1LTAxNDU5ZTY3ZGM5Yy9yZXNvdXJjZUdyb3Vwcy9UcmFuc1ZpZC9wcm92aWRlcnMvTWljcm9zb2Z0LkNvZ25pdGl2ZVNlcnZpY2VzL2FjY291bnRzL1RyYW5zVmlkIiwiaXNzIjoidXJuOm1zLmNvZ25pdGl2ZXNlcnZpY2VzIiwiYXVkIjoidXJuOm1zLm1pY3Jvc29mdHRyYW5zbGF0b3IiLCJleHAiOjE1MDg2Nzk5NTl9.Lp6jrIb2iXgNUhZtMZsyVNtS2FqvESYOLlakWSMvxwU";
+var youtubeUrl = "https://www.youtube.com/watch?v=";
+var vidId = "";
+chrome.tabs.getSelected(null, function (tab) {
+    vidId = tab.url.substring(youtubeUrl.length);
+    console.log(array);
+});
 
-var language = "en";
+var languageFrom = "fr";
+
+var googleUrl = "http://video.google.com/timedtext"; 
+var msURL = "https://api.microsofttranslator.com/V2/Http.svc";
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vYXBpLm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInN1YnNjcmlwdGlvbi1pZCI6IjY4ZDE3ODQ4YmRiMjRhNDlhZGM0YmE1NjJkOWIxMjVlIiwicHJvZHVjdC1pZCI6IlRleHRUcmFuc2xhdG9yLkYwIiwiY29nbml0aXZlLXNlcnZpY2VzLWVuZHBvaW50IjoiaHR0cHM6Ly9hcGkuY29nbml0aXZlLm1pY3Jvc29mdC5jb20vaW50ZXJuYWwvdjEuMC8iLCJhenVyZS1yZXNvdXJjZS1pZCI6Ii9zdWJzY3JpcHRpb25zLzBkMmZmOThjLTVkYjAtNGZiOC05MmI1LTAxNDU5ZTY3ZGM5Yy9yZXNvdXJjZUdyb3Vwcy9UcmFuc1ZpZC9wcm92aWRlcnMvTWljcm9zb2Z0LkNvZ25pdGl2ZVNlcnZpY2VzL2FjY291bnRzL1RyYW5zVmlkIiwiaXNzIjoidXJuOm1zLmNvZ25pdGl2ZXNlcnZpY2VzIiwiYXVkIjoidXJuOm1zLm1pY3Jvc29mdHRyYW5zbGF0b3IiLCJleHAiOjE1MDg2ODA5NjR9.HrPzy3xALPRO-8R3hgqLxlDwCPvfw0SGGbHZPUHYTOc";
+
+var languageTo = "en";
 
 var request = new XMLHttpRequest();
 request.addEventListener("load", loadScript);
 
-request.open("GET", videoUrl, true);
+request.open("GET", googleUrl + "?lang=" + languageFrom + "&v=" + vidId, true);
 request.send();
 
 function htmlDecode(input){
@@ -39,7 +48,7 @@ function translate(data) {
 function voiceOver(script) {
     var voice = document.getElementById("voice");
     voice.pause();
-    document.getElementById("voice_source").setAttribute("src", msURL + "/Speak?appid=Bearer "+ token + "&format=audio/mp3&language=" + language + "&text=" + script);
+    document.getElementById("voice_source").setAttribute("src", msURL + "/Speak?appid=Bearer "+ token + "&format=audio/mp3&language=" + languageTo + "&text=" + script);
     voice.load();
     voice.play();
 }
