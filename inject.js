3261504b6017543f5b77b157413adf4ce95ce0ff
Arden Kim <kmk3170@uw.edu>
2017-10-26 13:43:44 -0700

    no more mute button, now decrease volume

--- inject.js
+++ inject.js
@@ -2,9 +2,7 @@
 
         chrome.runtime.onMessage.addListener(
             function(request, sender, sendResponse) {
-              document.getElementsByClassName("video-stream html5-main-video")[0].currentTime = 6.5;
-              $('.ytp-mute-button').click();
-            //   console.log("hdshasa" + vid);
-            //   vid.currentTime = parseInt(request.greeting);
+              document.getElementsByClassName("video-stream html5-main-video")[0].currentTime = 5;
+              document.getElementsByClassName("video-stream html5-main-video")[0].volume = 0.03;
             }
         );
